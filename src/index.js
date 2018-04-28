ea94dab3fb9e4df9ca1881cfbaa7ca8e433c6fc1
owensource <ciruz001@cougars.csusm.edu>
2018-04-28 00:39:16 -0700

    router and components- mongo working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
+import AddItem from './components/AddItem';
+import IndexItem from './components/IndeItem';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route path='/add-item' component={AddItem} />
+            <Route path='/index' component={IndexItem} />
+        </div>
+    </Router>,
+     
+    document.getElementById('root')
+);
 registerServiceWorker();
